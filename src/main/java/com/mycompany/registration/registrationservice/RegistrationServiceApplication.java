c370c69048e52f861781f9280a8fcd059a03d19a
Colin But <colinbut@users.noreply.github.com>
2018-02-19 21:51:02 +0000

    spring-initializr generated project files

---
+++ src/main/java/com/mycompany/registration/registrationservice/RegistrationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.mycompany.registration.registrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationServiceApplication.class, args);
+	}
+}
