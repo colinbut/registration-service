dcc3fe0b11e85f8639d5f3720e4b34c7e5d532b9
Colin But <colinbut@users.noreply.github.com>
2018-03-01 21:42:16 +0000

    URL changes and logging

--- src/main/java/com/mycompany/registration/registrationservice/RegistrationEndpoint.java
+++ src/main/java/com/mycompany/registration/registrationservice/RegistrationEndpoint.java
@@ -5,7 +5,11 @@
  */
 package com.mycompany.registration.registrationservice;
 
+import com.mycompany.user.resource.UserResource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +20,12 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class RegistrationEndpoint {
 
-    private static final String USER_SERVICE_ENDPOINT = "/user/{ssn}";
+    private static final Logger LOGGER = LoggerFactory.getLogger(RegistrationEndpoint.class);
+
     private static final String USER_SERVICE_URL = "http://localhost:8080/";
 
-    public static final String USER_SERVICE_URL_ENDPOINT = USER_SERVICE_URL + USER_SERVICE_ENDPOINT;
+    private static final String USER_SERVICE_ENDPOINT = "/user/{ssn}";
+    private static final String USER_SERVICE_CREATE_USER_ENDPOINT = "/user/create";
 
     @Autowired
     private RestTemplate restTemplate;
@@ -27,13 +33,20 @@ public class RegistrationEndpoint {
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public ResponseEntity register() {
 
-        String userServiceUrlEndpoint = USER_SERVICE_URL_ENDPOINT.replace("{ssn}", "###-0000-###-0001");
+        String userServiceUrlEndpoint = USER_SERVICE_URL + USER_SERVICE_ENDPOINT.replace("{ssn}", "###-0000-###-0001");
+
+        LOGGER.debug("Calling external UserService with endpoint URL: %s", userServiceUrlEndpoint);
 
         ResponseEntity<String> responseEntity = restTemplate.getForEntity(userServiceUrlEndpoint, String.class);
 
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
+
+        UserResource requestResource = new UserResource();
+
+        HttpEntity<UserResource> request = new HttpEntity<>(requestResource);
+        restTemplate.postForEntity(USER_SERVICE_URL + USER_SERVICE_CREATE_USER_ENDPOINT, request, UserResource.class);
 
         return ResponseEntity.ok().build();
     }
