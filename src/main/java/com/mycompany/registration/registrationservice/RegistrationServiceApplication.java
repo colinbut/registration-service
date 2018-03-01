59fd1889f5d1c386c83f938299eb45d7dcb2513b
Colin But <colinbut@users.noreply.github.com>
2018-03-01 10:21:14 +0000

    reformat code

--- src/main/java/com/mycompany/registration/registrationservice/RegistrationServiceApplication.java
+++ src/main/java/com/mycompany/registration/registrationservice/RegistrationServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RegistrationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegistrationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationServiceApplication.class, args);
+    }
 }
