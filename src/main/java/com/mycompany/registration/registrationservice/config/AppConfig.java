5064a1f90a340fae42786672e5d29044f423ea01
Colin But <colinbut@users.noreply.github.com>
2018-03-07 13:11:21 +0000

    setting up RabbitMQ queues and exchanges configurations

--- src/main/java/com/mycompany/registration/registrationservice/config/AppConfig.java
+++ src/main/java/com/mycompany/registration/registrationservice/config/AppConfig.java
@@ -5,6 +5,10 @@
  */
 package com.mycompany.registration.registrationservice.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,8 +17,28 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
+    static final String MARKETING_QUEUE = "marketing-queue";
+    static final String EXCHANGE = "microservices-app-exchange";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(MARKETING_QUEUE, false);
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return new TopicExchange(EXCHANGE);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange topicExchange){
+        return BindingBuilder.bind(queue).to(topicExchange).with(MARKETING_QUEUE);
+    }
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }
+
+
 }
