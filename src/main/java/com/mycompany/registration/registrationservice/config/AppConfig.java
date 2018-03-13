14190c6e2514ed9fe39a0ffcfa2fc363184046c5
Colin But <colinbut@users.noreply.github.com>
2018-03-13 22:14:28 +0000

    configure rabbitmq correctly

--- src/main/java/com/mycompany/registration/registrationservice/config/AppConfig.java
+++ src/main/java/com/mycompany/registration/registrationservice/config/AppConfig.java
@@ -9,6 +9,9 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,8 +20,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class AppConfig {
 
-    static final String MARKETING_QUEUE = "registration-marketing-queue";
-    static final String EXCHANGE = "microservices-app-exchange";
+    public static final String MARKETING_QUEUE = "registration-marketing-queue";
+    public static final String EXCHANGE = "microservices-app-exchange";
 
     @Bean
     public Queue queue() {
@@ -35,10 +38,23 @@ public class AppConfig {
         return BindingBuilder.bind(queue).to(topicExchange).with(MARKETING_QUEUE);
     }
 
+    // Customize the configuration of RabbitTemplate rather than use the default one from Spring Boot
+    // because we need to configure the Jackson2MessageConverter in order to allow serialize java object to JSON on the wire
+    @Bean
+    public RabbitTemplate rabbitTemplate(final ConnectionFactory connectionFactory){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(producerJackson2MessageConverter());
+        return rabbitTemplate;
+    }
+
+    @Bean
+    Jackson2JsonMessageConverter producerJackson2MessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
         return restTemplateBuilder.build();
     }
-
 
 }
