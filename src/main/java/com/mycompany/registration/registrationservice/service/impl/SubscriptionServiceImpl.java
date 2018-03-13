0fb9abaa29afbbc633455b3a1dc6856f751c1ae0
Colin But <colinbut@users.noreply.github.com>
2018-03-13 22:14:57 +0000

    using rabbittemplate

--- src/main/java/com/mycompany/registration/registrationservice/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/mycompany/registration/registrationservice/service/impl/SubscriptionServiceImpl.java
@@ -5,14 +5,27 @@
  */
 package com.mycompany.registration.registrationservice.service.impl;
 
+import com.mycompany.registration.registrationservice.config.AppConfig;
 import com.mycompany.registration.registrationservice.model.Registration;
 import com.mycompany.registration.registrationservice.service.SubscriptionService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SubscriptionServiceImpl implements SubscriptionService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(SubscriptionServiceImpl.class);
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public void sendRegistration(Registration registration) {
-        throw new UnsupportedOperationException("Not Yet Implemented");
+        LOG.info("Sending registration %s to SubscriptionService", registration);
+
+        rabbitTemplate.convertAndSend(AppConfig.EXCHANGE, AppConfig.MARKETING_QUEUE, registration);
     }
 }
--- src/test/java/com/mycompany/registration/registrationservice/service/impl/SubscriptionServiceImplUTest.java
+++ src/test/java/com/mycompany/registration/registrationservice/service/impl/SubscriptionServiceImplUTest.java
@@ -5,19 +5,33 @@
  */
 package com.mycompany.registration.registrationservice.service.impl;
 
+import com.mycompany.registration.registrationservice.config.AppConfig;
+import com.mycompany.registration.registrationservice.model.Registration;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
-import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class SubscriptionServiceImplUTest {
+
+    @Mock
+    private RabbitTemplate rabbitTemplate;
 
     @InjectMocks
     private SubscriptionServiceImpl classUnderTest = new SubscriptionServiceImpl();
 
     @Test
     public void testSendRegistration() throws Exception {
-        fail("Not Yet Implemented");
+        Registration registration = new Registration();
+        Mockito.doNothing().when(rabbitTemplate).convertAndSend(AppConfig.EXCHANGE, AppConfig.MARKETING_QUEUE, registration);
+
+        classUnderTest.sendRegistration(registration);
+
+        Mockito.verify(rabbitTemplate, Mockito.times(1)).convertAndSend(AppConfig.EXCHANGE, AppConfig.MARKETING_QUEUE, registration);
     }
 
 }
